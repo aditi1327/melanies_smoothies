a87dbd1ca630f74e9d9df981bbd66136153cdb44
aditi1327 <athakre@peramalservices.com>
2025-09-03 17:13:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,13 +39,13 @@ if ingredients_list:
 
             st.write(ingredients_string)
 
-                my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
-                values ('""" + ingredients_string + """','"""+name_on_order+ """') """
+            my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+            values ('""" + ingredients_string + """','"""+name_on_order+ """') """
 
-                #st.write(my_insert_stmt)  
-                #st.stop()
+            #st.write(my_insert_stmt)  
+            #st.stop()
     
-                time_to_insert = st.button('submit order')
+            time_to_insert = st.button('submit order')
  
                 if time_to_insert:
                    session.sql(my_insert_stmt).collect()
