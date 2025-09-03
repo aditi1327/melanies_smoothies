ad2202538f99bae02ae848fc6fa3f4eddfff5bd7
aditi1327 <athakre@peramalservices.com>
2025-09-03 15:47:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,13 @@ ingredients_list = st.multiselect(
     ,max_selections = 5
 )
 if ingredients_list:
-   
    ingredients_string = ''
 
    for fruit_chosen in ingredients_list:
        ingredients_string += fruit_chosen +''
+       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+
    st.write(ingredients_string)
 
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
@@ -41,10 +43,7 @@ if ingredients_list:
    if time_to_insert:
        session.sql(my_insert_stmt).collect()
        st.success('Your Smoothie is ordered!', icon="✅")
-#New section to display smoothiefruit nutrition information 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-
-st.text(smoothiefroot_response.json())
 sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
 
