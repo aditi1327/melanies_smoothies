9b3b56a3736f0cedc955d9e970eb6c2cd456299a
aditi1327 <athakre@peramalservices.com>
2025-09-04 14:31:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,8 +50,8 @@ if ingredients_list:
     
             time_to_insert = st.button('submit order')
  
-   if time_to_insert:
-       session.sql(my_insert_stmt).collect()
-       st.success('Your Smoothie is ordered!', icon="✅")
+            if time_to_insert:
+               session.sql(my_insert_stmt).collect()
+               st.success('Your Smoothie is ordered!', icon="✅")
        
 
