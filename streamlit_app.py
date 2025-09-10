f3454056e4a0b0ac5a5a990c40f0fa5b2f6ebd7e
aditi1327 <athakre@peramalservices.com>
2025-09-10 15:42:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,7 @@ ingredients_list = st.multiselect(
     ,my_dataframe
     ,max_selections = 5
     )
+
 if ingredients_list:
     ingredients_string = ''
 
@@ -40,7 +41,7 @@ if ingredients_list:
             fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
             fv_df = st.dataframe(data=fruityvice_response.json(),use_container_width=True)
 
-    st.write(ingredients_string)
+    #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
     values ('""" + ingredients_string + """','"""+name_on_order+ """') """
